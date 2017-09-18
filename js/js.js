0aa717b21f3430c19bd3b0163937722dde651800
Joachim Wiberg <joachimpw@gmail.com>
2017-09-18 14:40:28 +0200

    update

--- js/js.js
+++ js/js.js
@@ -22,3 +22,30 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
+
+
+jQuery(document).ready(function(){
+	if( $('.cd-stretchy-nav').length > 0 ) {
+		var stretchyNavs = $('.cd-stretchy-nav');
+		
+		stretchyNavs.each(function(){
+			var stretchyNav = $(this),
+				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+			
+			stretchyNavTrigger.on('click', function(event){
+				event.preventDefault();
+				stretchyNav.toggleClass('nav-is-visible');
+			});
+		});
+
+		$(document).on('click', function(event){
+			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
+		});
+	}
+});
+
+$(function() {
+for (var i = 1; i <= 16; i++) {
+  $('div.loader').append('<span class="block-' + i + '"></span>');
+}
+};
