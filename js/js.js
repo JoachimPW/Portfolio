a3bc423681997c1d924a5391dd289857e915fb17
Joachim Wiberg <joachimpw@gmail.com>
2017-09-18 02:52:52 +0200

    gogo

---
+++ js/js.js
@@ -0,0 +1,24 @@
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "0 " + (windowYOffset * speed) + "px";
+      
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
